af1cfda66609aaea50237e370711342c5b77be85
Andrea Martella <andrea.martella@whiterabbit.cloud>
2022-07-02 21:34:02 +0200

    Updated intents

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('node:path');
 const { Client, Intents } = require('discord.js');
 require('dotenv').config();
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const eventsPath = path.join(__dirname, 'events');
 const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
