00e3198a331cc8a0af075c8f7b25f65d432f8b6e
Andrea Martella <andrea.martella@whiterabbit.cloud>
2022-07-02 21:22:11 +0200

    Updated the event handling

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,6 +1,9 @@
 module.exports = {
-	name: 'interactionCreate',
-	execute(interaction) {
-		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`);
+	name: "interactionCreate",
+	async execute(client, interaction) {
+		console.log(client);
+		console.log(
+			`${interaction.user.tag} in #${interaction.channel.name} triggered an interaction.`
+		);
 	},
 };
