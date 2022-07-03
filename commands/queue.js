fe583de68da7243cff18b8d85cc6d39e1ae17a77
Andrea Martella <andrea.martella@whiterabbit.cloud>
2022-07-03 04:14:48 +0200

    Fixes queue command

--- commands/queue.js
+++ commands/queue.js
@@ -1,25 +1,26 @@
+const { AudioPlayerStatus } = require("@discordjs/voice");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName("queue")
 		.setDescription("Elenca la coda di riproduzione"),
-	async execute(interaction, subscriptions, subscription) {		
+	async execute(interaction, subscriptions, subscription) {
 		// Print out the current queue, including up to the next 5 tracks to be played.
 		if (subscription) {
 			const current =
 				subscription.audioPlayer.state.status === AudioPlayerStatus.Idle
 					? `Non c'è nulla in riproduzione`
-					: `Ascoltando **${(subscription.audioPlayer.state.resource).metadata.title}**`;
+					: `Ascoltando **${subscription.audioPlayer.state.resource.metadata.title}**`;
 
 			const queue = subscription.queue
 				.slice(0, 5)
 				.map((track, index) => `${index + 1}) ${track.title}`)
-				.join('\n');
+				.join("\n");
 
 			await interaction.reply(`${current}\n\n${queue}`);
 		} else {
-			await interaction.reply('Non sto facendo ascoltare musica qui');
+			await interaction.reply("Non sto facendo ascoltare musica qui");
 		}
 	},
 };
