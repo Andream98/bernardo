e86cec137a659737b6039b754520ccacf02e97d5
Andrea Martella <andrea.martella@whiterabbit.cloud>
2022-07-03 04:17:21 +0200

    Updates skip command

--- commands/skip.js
+++ commands/skip.js
@@ -6,11 +6,12 @@ module.exports = {
 		.setDescription("Salta la canzone in riproduzione e passa alla prossima canzone in coda"),
 	async execute(interaction, subscriptions, subscription) {		
 		if (subscription) {
+			let currentSong = subscription.audioPlayer.state.resource.metadata.title;
 			// Calling .stop() on an AudioPlayer causes it to transition into the Idle state. Because of a state transition
 			// listener defined in music/subscription.ts, transitions into the Idle state mean the next track from the queue
 			// will be loaded and played.
 			subscription.audioPlayer.stop();
-			await interaction.reply('Canzone saltata');
+			await interaction.reply(`${currentSong} saltata`);
 		} else {
 			await interaction.reply('Ma si scem, non c\'è nulla in riproduzione');
 		}
